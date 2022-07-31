5100726fc78af2fc405c969346cd71c53d614dbb
dano <92238288+dano235@users.noreply.github.com>
2022-07-31 17:11:51 +0530

    Bug Patch #1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ level = 1
 
 while True:
     try:
-        random_number = 97
+        random_number = random.randint(0, 100)
         user_input = int(input("[GTN] Enter a number between 10-100: "))
 
         if user_input == random_number:
@@ -25,9 +25,12 @@ while True:
         elif user_input == 0:
             break
         else:
-            print("[GTN] You lose! Number was", random_number)
-            score -= 1
-            print(f"[GTN] Score: {score}")
+            print(f"[GTN] You lose! Number was {random_number}")
+            if score >= 1:
+                score -= 1
+            else: 
+                score = 0
+            print(f"[GTN] Score: {abs(score)}")
             with Bar("[GTN] Restarting", max = 20) as bar:
                 for i in range(20):
                     time.sleep(0.20)
