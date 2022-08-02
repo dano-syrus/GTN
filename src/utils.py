dafcc2cec762e4c82d35a901810483cbedc40197
dano <92238288+dano235@users.noreply.github.com>
2022-08-02 10:59:05 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
+questions = { "easy":"[GTN] Enter a number between 1-100: ", "medium":"[GTN] Enter a number between 1-300: ", "hard":"[GTN] Enter a number between 1-500: " }
 
 def banner():
+    os.system("clear")
     banner = open("./src/banner.txt","r")
     showbanner = banner.read()
     print(showbanner)
