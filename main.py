48471cc6de026548d7e9b920c5470fd3a960aaeb
dano <92238288+dano235@users.noreply.github.com>
2022-08-02 10:58:33 +0530

    Release v2.0

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from progress.bar import Bar
 from src import utils
 import random, os, time
 
-os.system("clear")
 utils.banner()
 score = 0
-level = 1
+difficulty = "easy"
 
+def select_difficulty():
+    global difficulty, random_number
+    user_input = input("[GTN] Select Difficulty: ")
+    if user_input.lower() == "easy":
+        difficulty = "easy"
+        random_number = random.randint(1, 100)
+    elif user_input.lower() == "medium":
+        difficulty = "medium"
+        random_number = random.randint(1, 300)
+    elif user_input.lower() == "hard":
+        difficulty = "hard"
+        random_number = random.randint(1, 500)
+
+select_difficulty()
 while True:
     try:
-        random_number = random.randint(0, 100)
-        user_input = int(input("[GTN] Enter a number between 10-100: "))
-
+        user_input = int(input(utils.questions[difficulty]))
         if user_input == random_number:
-            level += 1
             print("[GTN] You just won!")
             score += 1
             print(f"[GTN] Score: {score}")
-            with Bar(f"[GTN] Loading Level {level}", max = 20) as bar:
+            with Bar(f"[GTN] Loading", max = 20) as bar:
                 for i in range(20):
                     time.sleep(0.20)
                     bar.next()
@@ -30,11 +40,11 @@ while True:
                 score -= 1
             else: 
                 score = 0
-            print(f"[GTN] Score: {abs(score)}")
+            print(f"[GTN] Score: {score}")
             with Bar("[GTN] Restarting", max = 20) as bar:
                 for i in range(20):
                     time.sleep(0.20)
                     bar.next()
     except:
-        print("[GTN]: Something went wrong, try again! Type 0 to exit from the game.")
+        print("[GTN]: Something went wrong, try again!")
         os.system("exit")
