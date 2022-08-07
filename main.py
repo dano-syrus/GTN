dbf8d81a43017ad8d4a387995bc41cba0fc6e982
dano <92238288+dano235@users.noreply.github.com>
2022-08-07 20:59:53 +0530

    Release v2.1

--- main.py
+++ main.py
@@ -1,50 +1,37 @@
 from progress.bar import Bar
 from src import utils
-import random, os, time
+import os, time, random
 
 utils.banner()
-score = 0
-difficulty = "easy"
+score = 1
 
-def select_difficulty():
-    global difficulty, random_number
-    user_input = input("[GTN] Select Difficulty: ")
-    if user_input.lower() == "easy":
-        difficulty = "easy"
-        random_number = random.randint(1, 100)
-    elif user_input.lower() == "medium":
-        difficulty = "medium"
-        random_number = random.randint(1, 300)
-    elif user_input.lower() == "hard":
-        difficulty = "hard"
-        random_number = random.randint(1, 500)
-
-select_difficulty()
-while True:
+running = True
+while running:
     try:
-        user_input = int(input(utils.questions[difficulty]))
+        random_number = random.randint(1, 100)
+        user_input = int(input(utils.quest[100]))
         if user_input == random_number:
-            print("[GTN] You just won!")
+            print(f"{utils.prefix}You just won!")
             score += 1
-            print(f"[GTN] Score: {score}")
-            with Bar(f"[GTN] Loading", max = 20) as bar:
+            print(f"{utils.prefix}Score: {score}")
+            with Bar(f"{utils.prefix}Loading", max = 20) as bar:
                 for i in range(20):
-                    time.sleep(0.20)
+                    time.sleep(0.15)
                     bar.next()
             os.system("clear")
         elif user_input == 0:
             break
         else:
-            print(f"[GTN] You lose! Number was {random_number}")
+            print(f"{utils.prefix}You lose! Number was {random_number}")
             if score >= 1:
                 score -= 1
             else: 
                 score = 0
-            print(f"[GTN] Score: {score}")
-            with Bar("[GTN] Restarting", max = 20) as bar:
+            print(f"{utils.prefix}Score: {score}")
+            with Bar(f"{utils.prefix}Restarting", max = 20) as bar:
                 for i in range(20):
-                    time.sleep(0.20)
+                    time.sleep(0.15)
                     bar.next()
     except:
-        print("[GTN]: Something went wrong, try again!")
-        os.system("exit")
+        print(f"{utils.prefix}Something went wrong, try again!")
+        running = False
