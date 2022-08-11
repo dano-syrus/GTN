3323e4e4520e0fe1836528bf2b647d57f4868de8
dano <92238288+dano235@users.noreply.github.com>
2022-08-11 20:52:48 +0530

    Release v2.5

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,22 @@
-import os
-prefix = "[GTN] "
-quest = {100: f"{prefix}Enter a number between 1-100: "}
+from progress.bar import Bar
+import os, time, random
 
-def banner():
+prefix = "[GTN] "
+quest = { 100: f"{prefix}Enter a number between 1-100: ", 200: f"{prefix}Enter a number between 1-200: ", 300: f"{prefix}Enter a number between 1-300: ", 400: f"{prefix}Enter a number between 1-400: ", 500: f"{prefix}Enter a number between 1-500: "}
+
+def init():
+    with Bar(f"{prefix}Starting",max = 20) as bar:
+        for i in range(20):
+            time.sleep(0.15)
+            bar.next()
+
     os.system("clear")
     banner = open("./src/banner.txt","r")
     showbanner = banner.read()
     print(showbanner)
     banner.close()
+
+def random_quest():
+    array = [100,200,300,400,500]
+    var = random.randint(0, 4)
+    return array[var]
