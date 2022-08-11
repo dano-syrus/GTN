e367e3a5754768a7b162f019114c8fc3f74c040a
dano <92238288+dano235@users.noreply.github.com>
2022-08-11 20:52:21 +0530

    Release v2.5

--- main.py
+++ main.py
@@ -2,36 +2,32 @@ from progress.bar import Bar
 from src import utils
 import os, time, random
 
-utils.banner()
-score = 1
+utils.init()
+guesses = 0
 
-running = True
-while running:
+while guesses < 5:
     try:
-        random_number = random.randint(1, 100)
-        user_input = int(input(utils.quest[100]))
+        random_quest = utils.random_quest()
+        random_number = random.randint(1, random_quest)
+        user_input = int(input(utils.quest[random_quest]))
+        guesses += 1
         if user_input == random_number:
-            print(f"{utils.prefix}You just won!")
-            score += 1
-            print(f"{utils.prefix}Score: {score}")
-            with Bar(f"{utils.prefix}Loading", max = 20) as bar:
+            print(f"{utils.prefix}You guessed the number in {str(guesses)}")
+            with Bar(f"{utils.prefix}Restarting",max = 20) as bar:
                 for i in range(20):
                     time.sleep(0.15)
                     bar.next()
             os.system("clear")
-        elif user_input == 0:
+        if user_input == 0:
             break
         else:
-            print(f"{utils.prefix}You lose! Number was {random_number}")
-            if score >= 1:
-                score -= 1
-            else: 
-                score = 0
-            print(f"{utils.prefix}Score: {score}")
-            with Bar(f"{utils.prefix}Restarting", max = 20) as bar:
-                for i in range(20):
+            print(f"{utils.prefix}You didn't guess the number! Number was {str(random_number)}")
+            with Bar(f"{utils.prefix}Loading",max = guesses) as bar:
+                for i in range(guesses):
                     time.sleep(0.15)
                     bar.next()
     except:
         print(f"{utils.prefix}Something went wrong, try again!")
-        running = False
+
+if guesses == 5:
+    print(f"{utils.prefix}You're out of guesses! GAME OVER")
